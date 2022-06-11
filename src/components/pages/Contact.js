7de4bdd1a3bc6df7f9f18bca450709e69404cc55
Rodrigo <rodvalenciaa@gmail.com>
2022-06-11 12:09:43 -0700

    contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {validateEmail} from '../../utils/helpers'
 
-function Contact () {
+function ContactPage () {
+    const [formState, setFormState] = useState({name:'', email:'', message:''});
+    const [errorMessage, setErrorMessage] = useState('');
+    const {name, email, message} = formState;
+
+    function controlChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if(!isValid) {
+                setErrorMessage('Valid Email Required!');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is Required!`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({formState, [e.target.name]: e.target.value })
+        }
+    }
+
+    function controlSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div className='justify-content-center' id='contactDiv'>
+            <h2 data-testid='h1tag' className='contactH2'>Contact Me</h2>
+            <form className='justify-content-center' id='contactForm'>
+                <div className='mt-5'>
+                    <label htmlFor='name'>Name:</label>
+                    <input className='formInput' type='text' name='name' defaultValue={name} onBlur={controlChange}></input>
+                </div>
+                <div className='mt-5'>
+                    <label htmlFor='email'>Email Address:</label>
+                    <input className='formInput' type='email' name='email' defaultValue={email} onBlur={controlChange}></input>
+                </div>
+                <div className='mt-5'>
+                    <label htmlFor='message'>Message:</label>
+                    <input className='formInput' name='message' defaultValue={message} onBlur={controlChange}></input>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className='errorMessage'>{errorMessage}</p>
+                    </div>
+                )}
+                <div className='mt-5 mb-5'>
+                    <button data-testid='button' className='btn btn-outline-dark mt-4' type='submit' onSubmit={controlSubmit}>Submit</button>
+                </div>
+
+            </form>
+            
         </div>
-    )
+    );
 }
 
-export default Contact;
+export default ContactPage;
