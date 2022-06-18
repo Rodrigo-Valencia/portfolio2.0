b7aca6e532b100c5436aec03ecc3f0f948c88513
Rodrigo <rodvalenciaa@gmail.com>
2022-06-17 19:08:18 -0700

    img

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -4,13 +4,14 @@ function ResumeLink(props) {
     return (
         <div className="card">
             <div className="imgContainer">
-                <img alt={props.name} src={props.img}></img>
+                <p>
+                    <a href=""> <img src="https://raw.githubusercontent.com/Rodrigo-Valencia/portfolio2.0/main/src/assets/profileImg.jpg"></img></a>
+                </p>
             </div>
             <div className="resumeContent">
                 <p className="resumeTitle" id="linkTitle"> {props.name} </p>
                 <p id="resumeIcons">
-                    <a href={props.github}> <img src=''></img> </a>
-                    <a href={props.launch}> <img src=''></img> </a>
+                    <a href={props.github}> <img src='https://img.icons8.com/material-outlined/24/undefined/github.png'></img> </a>
                 </p>
             </div>
         </div>
